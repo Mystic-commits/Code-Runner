732b1f348382499162071b54ed185e723fea9e3b
Mystic-commits <yugjohri520@gmail.com>
2025-02-10 00:58:40 +0530

    Commit one

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const LANGUAGE_VERSIONS = {
+  javascript: "18.15.0",
+  typescript: "5.0.3",
+  python: "3.10.0",
+  java: "15.0.2",
+  csharp: "6.12.0",
+  php: "8.2.3",
+};
+
+export const CODE_SNIPPETS = {
+  javascript: `\nfunction greet(name) {\n\tconsole.log("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet("Alex");\n`,
+  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hiii " + data.name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet({ name: "Alex" });\n`,
+  python: `\ndef greet(name):\n\tprint("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it")\n\ngreet("Alex")\n`,
+  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hiii Alex, Yug This side I hope you enjoy this project and find good use for it");\n\t}\n}\n`,
+  csharp:
+    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hiii Alex, Yug This side I hope you enjoy this project and find good use for it");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n\n$name = 'Alex';\necho 'Hiii ' . $name . ', Yug This side I hope you enjoy this project and find good use for it';\n",
+};
