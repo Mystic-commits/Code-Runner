a41075cb05d1aab232e04de12080c55dce006fec
Yug Johri <yugjohri520@gmail.com>
2025-02-10 16:14:53 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -8,11 +8,11 @@ export const LANGUAGE_VERSIONS = {
 };
 
 export const CODE_SNIPPETS = {
-  javascript: `\nfunction greet(name) {\n\tconsole.log("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet("Alex");\n`,
-  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hiii " + data.name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet({ name: "Alex" });\n`,
-  python: `\ndef greet(name):\n\tprint("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it")\n\ngreet("Alex")\n`,
-  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hiii Alex, Yug This side I hope you enjoy this project and find good use for it");\n\t}\n}\n`,
+  javascript: `\nfunction greet(name) {\n\tconsole.log("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet("Anu");\n`,
+  typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hiii " + data.name + ", Yug This side I hope you enjoy this project and find good use for it");\n}\n\ngreet({ name: "Anu" });\n`,
+  python: `\ndef greet(name):\n\tprint("Hiii " + name + ", Yug This side I hope you enjoy this project and find good use for it")\n\ngreet("Anu")\n`,
+  java: `\npublic class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("Hiii Anu, Yug This side I hope you enjoy this project and find good use for it");\n\t}\n}\n`,
   csharp:
-    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hiii Alex, Yug This side I hope you enjoy this project and find good use for it");\n\t\t}\n\t}\n}\n',
-  php: "<?php\n\n$name = 'Alex';\necho 'Hiii ' . $name . ', Yug This side I hope you enjoy this project and find good use for it';\n",
+    'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hiii Anu, Yug This side I hope you enjoy this project and find good use for it");\n\t\t}\n\t}\n}\n',
+  php: "<?php\n\n$name = 'Anu';\necho 'Hiii ' . $name . ', Yug This side I hope you enjoy this project and find good use for it';\n",
 };
